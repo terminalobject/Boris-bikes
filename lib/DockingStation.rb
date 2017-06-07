d432c01c39548761fc346afa2caf29c04d959dde
Miho Tse <mihotse@gmail.com>
2017-06-07 15:44:36 +0100

    Using Instance Variables

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,12 +1,16 @@
 class DockingStation
-  # attr_accessor :bike
-  #
-  # def initialize
-  #   @bike = nil
-  # end
+  attr_reader :bike
+
+  def initialize
+    @bike = bike
+  end
 
   def release_bike
     bike = Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
   end
 
 end
--- spec/Feature_tests_to_Unit_tests_spec.rb
+++ spec/Feature_tests_to_Unit_tests_spec.rb
@@ -9,4 +9,17 @@ describe DockingStation do
       expect(bike).to be_working
   end
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns the bike we dock' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
